893ca70f55fece6a7f22bb968f75f24c5ff5fa17
Mokhethi Nyapholi <nyapholimokhethi85@gmail.com>
2026-01-15 16:42:48 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-2016578123-3a5e9",
+  "appId": "1:857946052257:web:21066500b0bf1da26402e0",
+  "apiKey": "AIzaSyBSBY24xtIM6bhJ0z0HEZ4Y7buvIrDQx9I",
+  "authDomain": "studio-2016578123-3a5e9.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "857946052257"
+};
